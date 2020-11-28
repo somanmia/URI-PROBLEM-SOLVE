77051ed004d50e012d1cabb876d0692c1304b9e4
Md:Soman Mia <59284020+somanmia@users.noreply.github.com>
2020-11-28 03:59:49 -0800

    Create URI_2581.java

---
+++ URI_2581.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int i;
+        String  x="";
+        int N=input.nextInt();
+        for(i=0;i<N;i++){
+             x=input.nextLine();
+             input.next();
+             System.out.println("I am Toorg!");
+        }
+    }
+
+}
